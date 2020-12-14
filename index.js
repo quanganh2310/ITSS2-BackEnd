10dc9ee16ba0517fe1ec5fe52470809f68f635ab
quanganh2310 <ailavip1st@gmail.com>
2020-12-14 10:20:05 +0700

    sprint3

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 const express = require('express')
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
 const app = express()
 const port = 8000
 const router = require('./routers/router')
+
+// var corsOptions = {
+//   origin: "http://localhost:8000"
+// };
+
+// app.use(cors(corsOptions));
+
+// parse requests of content-type - application/json
+app.use(bodyParser.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.urlencoded()); app.use(express.json());
 
@@ -21,3 +36,60 @@ app.listen(process.env.PORT || port, () => {
                 localhost:8000/palace/:id => get palace by id            
               `)
 })
+
+const db = require("./models");
+const dbConfig = require("./config/db.config")
+const Role = db.role;
+
+db.mongoose
+  .connect(`mongodb://${dbConfig.HOST}:${dbConfig.PORT}/${dbConfig.DB}`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log("Successfully connect to MongoDB.");
+    initial();
+  })
+  .catch(err => {
+    console.error("Connection error", err);
+    process.exit();
+  });
+
+function initial() {
+  Role.estimatedDocumentCount((err, count) => {
+    if (!err && count === 0) {
+      new Role({
+        name: "user"
+      }).save(err => {
+        if (err) {
+          console.log("error", err);
+        }
+
+        console.log("added 'user' to roles collection");
+      });
+
+      new Role({
+        name: "moderator"
+      }).save(err => {
+        if (err) {
+          console.log("error", err);
+        }
+
+        console.log("added 'moderator' to roles collection");
+      });
+
+      new Role({
+        name: "admin"
+      }).save(err => {
+        if (err) {
+          console.log("error", err);
+        }
+
+        console.log("added 'admin' to roles collection");
+      });
+    }
+  });
+}
+
+require('./routers/auth')(app);
+require('./routers/user')(app);
